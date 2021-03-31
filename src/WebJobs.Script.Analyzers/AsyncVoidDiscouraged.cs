2ef5a0de59a8a967cb63300c417340f3d272c21b
Katy Shimizu <kashimiz@microsoft.com>
2021-03-30 23:21:13 -0700

    Created analyzer project and initial framework.

---
+++ src/WebJobs.Script.Analyzers/AsyncVoidDiscouraged.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace WebJobs.Script.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]  // TODO: should these work on PowerShell?
+    public class AsyncVoidDiscouraged : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(DiagnosticDescriptors.AsyncVoidDiscouraged); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            
+            // TODO: use CompilationStartAction to only register analyzer on Function methods?
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var methodSymbol = (IMethodSymbol)context.Symbol;
+
+            if (!methodSymbol.IsFunction(context))
+            {
+                return;
+            }
+
+            if (!methodSymbol.IsAsync)
+            {
+                return;
+            }
+
+            if (methodSymbol.ReturnsVoid)
+            {
+                var diagnostic = Diagnostic.Create(DiagnosticDescriptors.AsyncVoidDiscouraged, methodSymbol.Locations[0]);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ src/WebJobs.Script.Analyzers/DiagnosticDescriptors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace WebJobs.Script.Analyzers
+{
+    internal class DiagnosticDescriptors
+    {
+        private static DiagnosticDescriptor Create(string id, string title, string messageFormat, string category, DiagnosticSeverity severity)
+        {
+            // TODO: needs different site/code
+            string helpLink = $"https://aka.ms/azfw-rules?ruleid={id}"; 
+            return new DiagnosticDescriptor(id, title, messageFormat, category, severity, isEnabledByDefault: true, helpLinkUri: helpLink);
+        }
+
+        public static DiagnosticDescriptor AsyncVoidDiscouraged { get; }
+            = Create(id: "AZFX0001", title: "Async void discouraged", 
+                messageFormat: "Async void can lead to unexpected behavior. Return Task instead.",
+                category: Constants.DiagnosticsCategories.Usage,
+                severity: DiagnosticSeverity.Warning); // TODO: Should this be warning or erro?
+    }
+}
